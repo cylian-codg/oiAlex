96c9acc02cb87e58ba46a11dbb57498e68ea72e6
Cylian Lins Mendes Melo <82554933+cylian-codg@users.noreply.github.com>
2023-09-19 14:55:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var cylian = createSprite(200,200,20,20);
+
+function setup() {
+
+  createCanvas(400,400);
+  
+  console.log("oi alex");
+}
+
+function draw() 
+{
+  background(30);
+
+
+  drawSprites();
+
+}
+
+
+
+
